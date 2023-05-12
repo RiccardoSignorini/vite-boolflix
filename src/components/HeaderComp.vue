0a39803eaf9e0b6fd15ae2941144367fd1ba484c
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-05-12 15:36:37 +0200

    modifica barra di ricerca e pulsante

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -18,7 +18,9 @@
         </div>
         <div>
             <input type="text" placeholder="Cerca film o serie TV..." v-model="store.inputSearch" @keyup="$emit('startSearch')">
-            <button class="m-3" @click="$emit('startSearch')">Search</button>
+            <button class="m-3" @click="$emit('startSearch')">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
         </div>    
     </div>
 </template>
@@ -32,7 +34,9 @@
         }
         
         button, input{
-            border: 0;
+            border: 1px solid white;
+            padding: 10px;
+            border-radius: 15px;
         }    
     }
 </style>
